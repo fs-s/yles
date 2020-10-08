0102c463fb5d1ae2980a97ecc9e4592da9a94700
fs-s <joonas.puuding@ametikool.ee>
2020-10-08 15:32:19 +0300

    random lessons

---
+++ app.js
@@ -0,0 +1,34 @@
+////calc
+//
+//let kroon = 1000
+//const eur = 15.6466
+//
+//console.log(Math.round(kroon / eur * 100) / 100)
+//
+//
+////radius
+//
+//const radius = 10
+//
+//console.log(Math.round(Math.PI * radius * radius * 100) / 100)
+//
+//// 1-9
+//
+
+
+
+
+
+let a = 1
+let b = a.toString() + a.toString()
+let c = a.toString() + a + a
+let sum = a + parseInt(b) + parseInt(c)
+
+console.log(a + " + " + b + " + " + c + " = " + sum)
+
+// min
+
+//let number1 = 5
+//let number2 = 7
+//
+//console.log(Math.min(number1, number2))
